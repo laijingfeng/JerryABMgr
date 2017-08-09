ebd8928f3f2dae862a7adef3c7304deb5ba53bf3
laijingfeng <337292616@qq.com>
2017-08-09 12:03:50 +0800

    update

---
+++ Assets/Code/TestCompressUtil.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCompressUtil
+{
+    public static List<string> TestFiles
+    {
+        get
+        {
+            return new List<string>()
+            {
+                "general_assets_bundle",
+                "pet005_bundle",
+            };
+        }
+
+    }
+
+    public static string OringinDir
+    {
+        get
+        {
+            return Application.dataPath + "/../OriginAssets/Android/";
+        }
+    }
+
+    public static string StreamingAssets
+    {
+        get
+        {
+            return Application.streamingAssetsPath + "/";
+        }
+    }
+
+    public static string PersistentDataPath
+    {
+        get
+        {
+            return Application.dataPath + "/PersistentDataPath/";
+        }
+    }
+}
--- Assets/Code/UITest.cs
+++ Assets/Code/UITest.cs
@@ -6,24 +6,79 @@ public class UITest : MonoBehaviour
 {
     private Text txt;
     private Slider slider;
+    private Button compressBtn;
+    private Button workBtn;
 
     void Awake()
     {
         slider = this.transform.FindChild("Slider").GetComponent<Slider>();
         txt = this.transform.FindChild("Text").GetComponent<Text>();
+        
+        compressBtn = this.transform.FindChild("Compress").GetComponent<Button>();
+        compressBtn.onClick.AddListener(Compress);
+
+        workBtn = this.transform.FindChild("Work").GetComponent<Button>();
+        workBtn.onClick.AddListener(() =>
+        {
+            this.StartCoroutine(IE_Work());
+        });
     }
 
     void Start()
     {
-        this.StartCoroutine(IE_Work());
     }
+
+    #region MultiCompress
+
+    private MultiCompress multiCompress = null;
+    private float multiCompressStartTime = 0;
+
+    private void Compress()
+    {
+        multiCompress = new MultiCompress();
+        multiCompress.SetCallback((finish, total, status) =>
+        {
+            float rate = finish * 1.0f / total;
+            txt.text = string.Format("Compressing {0:G}/{1:G} {2:F2}% {3:F2}", finish, total, rate * 100, Time.realtimeSinceStartup - multiCompressStartTime);
+            slider.value = rate;
+        });
+        foreach (string file in TestCompressUtil.TestFiles)
+        {
+            multiCompress.AddCompressConfig(new CompressConfig()
+            {
+                inFile = TestCompressUtil.OringinDir + file,
+                outFile = TestCompressUtil.OringinDir + CompressUtil.GetCompressFileName(file),
+            });
+        }
+        multiCompressStartTime = Time.realtimeSinceStartup;
+        multiCompress.Start();
+        this.StartCoroutine("IE_UpdateMultiCompress");
+    }
+
+    private IEnumerator IE_UpdateMultiCompress()
+    {
+        while (multiCompress != null)
+        {
+            multiCompress.UpdateCallback();
+            if (multiCompress.Status == CompressState.Finish)
+            {
+                multiCompress.UpdateCallback();
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+        }
+        txt.text = "Finish";
+    }
+
+    #endregion MultiCompress
 
     private IEnumerator IE_Work()
     {
         txt.text = "Ready";
         yield return this.StartCoroutine(IE_Load());
         yield return new WaitForSeconds(0.2f);
-        yield return this.StartCoroutine(IE_Compress());
+        yield return this.StartCoroutine(IE_Decompress());
         yield return new WaitForSeconds(0.2f);
         txt.text = "Finish";
     }
@@ -46,7 +101,7 @@ public class UITest : MonoBehaviour
         }
     }
 
-    private IEnumerator IE_Compress()
+    private IEnumerator IE_Decompress()
     {
         int tar = 50;
         float rate = 0;
@@ -54,7 +109,7 @@ public class UITest : MonoBehaviour
         for (int i = 0; i <= tar; i++)
         {
             rate = i * 1.0f / tar;
-            txt.text = string.Format("Compressing {0:G}/{1:G} {2:F2}% {3:F2}", i, tar, rate * 100, Time.realtimeSinceStartup - stime);
+            txt.text = string.Format("Decompressing {0:G}/{1:G} {2:F2}% {3:F2}", i, tar, rate * 100, Time.realtimeSinceStartup - stime);
             slider.value = rate;
             if (i == tar)
             {
